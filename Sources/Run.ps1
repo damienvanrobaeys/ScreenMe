9ede3fb93ce2616f2a2e2f74c681bbfe33eee6a8
Damien VAN ROBAEYS <damien.vanrobaeys@gmail.com>
2020-03-26 15:47:11 +0100

    Add files via upload

---
+++ Sources/Run.ps1
@@ -0,0 +1,6 @@
+start-process powershell .\ScreenMe.ps1
+# start-process powershell "C:\Users\damien.van robaeys\Desktop\Posts DVR\Screen_Me\Sources_Tool_v2\ScreenMe.ps1"
+
+# $temp_folder = $env:temp
+# powershell.exe -sta -executionpolicy Bypass -WindowStyle Hidden -file "$temp_folder\ScreenMe.ps1"
+# powershell -sta .\ScreenMe.ps1
